237352df5131c029d966ad63cde7a079fca945ea
sarah <s.j.condon01@gmail.com>
2019-10-03 22:13:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
 # Add your code here
+class Dog
+  @@all = []
+
+def initialize(name)
+  @name = name
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def self.print_all
+ self.all.each{|dog| puts "#{dog.name}"}
+end
+
+def self.save
+  @@all << self
+end
+
+def self.clear_all
+  @@all.clear
+end
+end
+
